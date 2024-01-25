96ae9122f3c99ce2bba0d37bdcfcf81caaea48e9
shaimaa <shaimafikry@yahoo.com>
2024-01-26 00:02:49 +0200

    add new task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -36,7 +36,7 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		/*loop to search for the most lower value*/
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
