f33e268f5488041b8fd9160e287d84d2d64e43c1
shaimaa <shaimafikry@yahoo.com>
2024-01-25 23:49:32 +0200

    add new task

--- sort.h
+++ sort.h
@@ -20,49 +20,8 @@ typedef struct listint_s
 } listint_t;
 
 
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-	int i;
-
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
-}
-
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}
-
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
