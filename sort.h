765a81d929dd10332de7bf4b3346524699bd9899
shaimaa <shaimafikry@yahoo.com>
2024-01-25 23:50:35 +0200

    add new task

--- sort.h
+++ sort.h
@@ -27,5 +27,5 @@ void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void swap_node(listint_t *a, listint_t *b);
 #endif
