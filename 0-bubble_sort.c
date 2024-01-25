ae899560b8c6a042e7b52b49bbb27562626d2b58
shaimaa <shaimafikry@yahoo.com>
2024-01-25 20:45:50 +0200

    add new task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * quick_sort: sotrs elemnt in quic sort
+ * @array: pointer to array
+ * @size: size of array
+ * Return: no return
+*/
+
+
+
+void bubble_sort(int *array, size_t size)
+{
+	/*define varaibles*/
+	size_t i, j;
+
+	/*check for error handling*/
+	if (!array || size == 1)
+		return;
+	/*loop through the array*/
+	for ( i = 0; i < size; i++)
+	{
+		/*loop to search for the most lower value*/
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1 ]);
+				print_array(array, size);
+
+			}
+		}
+
+	}
+}
--- main.c
+++ main.c
@@ -26,10 +26,10 @@ void swap (int *a, int *b)
 }
 
 
-void quick_sort(int *array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
 	/*define varaibles*/
-	size_t i, min, j;
+	size_t i, j;
 
 	/*check for error handling*/
 	if (!array || size == 1)
@@ -37,38 +37,32 @@ void quick_sort(int *array, size_t size)
 	/*loop through the array*/
 	for ( i = 0; i < size; i++)
 	{
-		/*assign the first value to min*/
-		min = i;
 		/*loop to search for the most lower value*/
-		for (j = i; j < size; j++)
+		for (j = 0; j < size; j++)
 		{
-			if (array[min] > array[j])
+			if (array[j] > array[j + 1])
 			{
-				min = j;
+				swap(&array[j], &array[j + 1 ]);
+				print_array(array, size);
+
 			}
 		}
 
-		
-		if (min != i)
-		{
-			swap(&array[min], &array[i]);
-			print_array(array, size);
-		}
-		
 	}
 }
+
 
 int main(void)
 {
 	clock_t start, end;
 	double cpu_time_used;
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86,5,9,87,78,546654,2,21,56,56,56,689,4,979,6, 7};
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
 	printf("\n");
 	start = clock();
-	quick_sort(array, n);
+    bubble_sort(array, n);
 	end = clock();
 	cpu_time_used = ((double) (end- start)) / CLOCKS_PER_SEC;
 	printf("exc time %f\n", cpu_time_used);
